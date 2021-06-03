b00bf7b3be6aa11b1ea8e68822589bacee2db128
Andrew L Fairchild <andrefairchild82418@gmail.com>
2021-06-03 23:54:56 +0000

    Created object of lists

---
+++ script.js
@@ -0,0 +1,32 @@
+const randomNumber = (num) => {
+  return Math.floor(Math.random() * num);
+};
+
+const clueAnswers = {
+  person: [
+    "Professor Plum",
+    "Miss Scarlett",
+    "Dwayne The Rock Johnson",
+    "Colonel Mustard",
+    "Mrs. White",
+    "Mrs. Peacock",
+    "R. Kelly",
+  ],
+  place: [
+    "Ballroom",
+    "Conservatory",
+    "Study",
+    "The Octogon",
+    "The Room With a Tarp on the Floor",
+    "The Kitchen",
+    "Dining Room"
+  ],
+  thing: [
+    "Revolver",
+    "Lead Pipe",
+    "Knife",
+    "Super Soaker Filled with Pee",
+    "Peoples Elbow",
+    "Rope"
+]
+};
